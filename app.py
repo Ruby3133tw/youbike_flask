8114908eba8be341cadfda5248dbda6c3bb22a51
Ruby3133tw <163227368+Ruby3133tw@users.noreply.github.com>
2025-06-08 17:22:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -164,5 +164,8 @@ def index():
         order=order
     )
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
